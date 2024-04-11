49f3b2151279a4b2ca5af1d2a896430f91c2fbc8
Jules Mons <33626268+julesmons@users.noreply.github.com>
2024-04-11 10:40:07 +0200

    Update prepare-mariadb.sh

--- prepare-mariadb.sh
+++ prepare-mariadb.sh
@@ -5,7 +5,7 @@
 wget -qO /etc/apt/trusted.gpg.d/mariadb.pgp https://mariadb.org/mariadb_release_signing_key.pgp
 
 ## Create package list
-echo "deb https://mirror.mariadb.org/repo/11.3/ubuntu `lsb_release -cs` mariadb main"
+echo "deb https://mirror.mariadb.org/repo/11.4/ubuntu `lsb_release -cs` mariadb main"
 
 ## Pin package to make sure the MariaDB repo is used
 echo -e "Package: *\nPin: origin mirror.mariadb.org\nPin: release o=mariadb-server\nPin-Priority: 900\$
